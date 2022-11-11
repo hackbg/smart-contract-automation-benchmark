62a7346b7885e9d2099ced102d728ea09f84da15
imollov <ivomollov@gmail.com>
2022-11-11 17:52:18 +0200

    feat: add RapidFire contract

---
+++ contracts/RapidFire.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+import "./interfaces/IConditionalCommand.sol";
+
+contract RapidFire is IConditionalCommand, AutomationCompatible {
+    function exec(bytes32 network) public {
+        emit Executed(network);
+    }
+
+    function shouldExec() public pure returns (bool) {
+        return true;
+    }
+
+    // CHAINLINK AUTOMATION
+
+    function checkUpkeep(bytes calldata)
+        external
+        pure
+        override
+        returns (bool upkeepNeeded, bytes memory)
+    {
+        upkeepNeeded = shouldExec();
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        exec("CHAINLINK");
+    }
+
+    // GELATO OPS
+
+    function checker()
+        external
+        pure
+        returns (bool canExec, bytes memory execPayload)
+    {
+        canExec = shouldExec();
+        execPayload = abi.encodeWithSelector(this.exec.selector, "GELATO");
+    }
+}
---
+++ test/RapidFire.test.ts
@@ -0,0 +1,76 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const { formatBytes32String } = ethers.utils;
+const { HashZero } = ethers.constants;
+
+describe("RapidFire", function () {
+  const testNetwork = formatBytes32String("TEST");
+
+  async function deployRapidFireFixture() {
+    const [onwer] = await ethers.getSigners();
+
+    const RapidFire = await ethers.getContractFactory("RapidFire");
+    const rapidFire = await RapidFire.deploy();
+
+    return { rapidFire, onwer };
+  }
+
+  describe("Condition", function () {
+    it("should be true", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      expect(await rapidFire.shouldExec()).to.be.true;
+    });
+  });
+
+  describe("Command", function () {
+    it("should not fail if called repeatedly", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      await rapidFire.exec(testNetwork);
+
+      expect(rapidFire.exec(testNetwork)).to.not.be.reverted;
+    });
+
+    it("should emit event on execution", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      await expect(rapidFire.exec(testNetwork))
+        .to.emit(rapidFire, "Executed")
+        .withArgs(testNetwork);
+    });
+  });
+
+  describe("Chainlink Automation", function () {
+    it("should perform upkeep with network name", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      await expect(rapidFire.performUpkeep(HashZero))
+        .to.emit(rapidFire, "Executed")
+        .withArgs(formatBytes32String("CHAINLINK"));
+    });
+  });
+
+  describe("Gelato Ops", function () {
+    it("should be true when checking if task should be executed", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      const [canExec] = await rapidFire.checker();
+
+      expect(canExec).to.be.true;
+    });
+
+    it("should return exec selector with network name arg", async function () {
+      const { rapidFire } = await loadFixture(deployRapidFireFixture);
+
+      const [, execPayload] = await rapidFire.checker();
+
+      const execWithGelatoEncoded =
+        "0xb5e98b3b0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000647454c41544f0000000000000000000000000000000000000000000000000000";
+
+      expect(execPayload).to.eq(execWithGelatoEncoded);
+    });
+  });
+});
