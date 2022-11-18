bfd3c92994ed1da07271cd3a52987a2376cd0b7f
imollov <ivomollov@gmail.com>
2022-11-18 13:43:22 +0200

    docs: add contract specs

--- contracts/Highlander.sol
+++ contracts/Highlander.sol
@@ -3,16 +3,36 @@ pragma solidity 0.8.17;
 
 import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
+/**
+ * @notice This is a benchmark contract part of a performance comparison of
+ * smart contract automation solutions. It is desigend such that there can be only
+ * one winner per servicing opportunity. There is a new servicing opportunity
+ * every t seconds.
+ */
 contract Highlander is AutomationCompatible {
+    // Interval of seconds between servicing opportunities
     uint256 public immutable i_interval;
+
+    // Last execution time
     uint256 public s_lastTimestamp;
 
+    /**
+     * @notice Captures an execution with details required to compare solutions
+     * @param success indicates wether the execution was within the target window
+     * @param network name of competitor solution servicing the contract
+     */
     event Executed(bool indexed success, bytes32 indexed network);
 
     constructor(uint256 interval) {
         i_interval = interval;
     }
 
+    /**
+     * @notice Command that needs to be serviced by the competing automation solutions
+     * @dev Even if the servicing opportunity has been  missed the tx does not revert and
+     * always emits an event so it can be easily queried.
+     * @param network Name of the solution servicing the contract
+     */
     function exec(bytes32 network) public {
         bool success = shouldExec();
 
@@ -23,6 +43,10 @@ contract Highlander is AutomationCompatible {
         }
     }
 
+    /**
+     * @notice The condition based on which solutions trigger execution
+     * @return Indicates wether the contract should be serviced
+     */
     function shouldExec() public view returns (bool) {
         return block.timestamp - s_lastTimestamp > i_interval;
     }
--- contracts/Target.sol
+++ contracts/Target.sol
@@ -3,10 +3,24 @@ pragma solidity 0.8.17;
 
 import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
+/**
+ * @notice This is a benchmark contract part of a performance comparison of
+ * smart contract automation solutions. It has a target or window of opportunity
+ * and tests the ability to execute in timely manner.
+ */
 contract Target is AutomationCompatible {
+    // Interval of blocks between each servicing opportunity
     uint256 public immutable i_interval;
+
+    // Number of blocks defining the window of opportunity
     uint256 public immutable i_window;
 
+    /**
+     * @notice Captures an execution with details required to compare solutions
+     * @dev Latency is measured by the tx block number in the dashboard query
+     * @param success indicates wether the execution was within the target window
+     * @param network name of competitor solution servicing the contract
+     */
     event Executed(bool indexed success, bytes32 indexed network);
 
     constructor(uint256 interval, uint256 window) {
@@ -14,11 +28,21 @@ contract Target is AutomationCompatible {
         i_window = window;
     }
 
+    /**
+     * @notice Command that needs to be serviced by the competing automation solutions
+     * @dev Even if the target has been missed the tx does not revert and always
+     * emits an event so it can be easily queried.
+     * @param network Name of the solution servicing the contract
+     */
     function exec(bytes32 network) public {
         bool success = shouldExec();
         emit Executed(success, network);
     }
 
+    /**
+     * @notice The condition based on which solutions trigger execution
+     * @return Indicates wether the contract should be serviced
+     */
     function shouldExec() public view returns (bool) {
         return block.number % i_interval <= i_window;
     }
